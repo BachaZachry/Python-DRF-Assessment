6f125339bf0e010e2ed66bb9dda226c5e9b65463
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2022-02-21 15:50:51 +0100

    Added the max number of airplanes restriction

--- airplanes/api/views.py
+++ airplanes/api/views.py
@@ -1,30 +1,37 @@
+from django.http import HttpRequest, HttpResponse
+
+from airplanes.models import Airplane
 from .serializers import AirplaneSerializer
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 import math
+from typing import TypeVar
+
+AirplaneType = TypeVar('AirplaneType', bound=Airplane)
 
 
 class AirplaneManagement(generics.GenericAPIView):
     serializer_class = AirplaneSerializer
     permission_classes = [permissions.AllowAny]
+    queryset = Airplane.objects.all()
 
-    def post(self, request):
+    def post(self, request: HttpRequest) -> HttpResponse:
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        airplane = serializer.save()
+        airplane: AirplaneType = serializer.save()
         # Calculating the consumption per minute
-        base_consumption_nolog = airplane.airplane_id * \
+        base_consumption_nolog: float = airplane.airplane_id * \
             airplane.fuel_consumption_multiplier
-        base_consumption = math.log(base_consumption_nolog, 10)
+        base_consumption: float = math.log(base_consumption_nolog, 10)
         # Calculating the total consumption by adding the passengers multiplier
-        total_consumption = base_consumption + \
+        total_consumption: float = base_consumption + \
             (airplane.passengers *
              airplane.passenger_fuel_increase)
         # Calculating the fuel tank capacity
-        fuel_tank_capacity = airplane.airplane_id * \
+        fuel_tank_capacity: int = airplane.airplane_id * \
             airplane.fuel_tank_capacity_multiplier
         # Calculating max minutes that the airplane can fly
-        max_minutes_flyable = fuel_tank_capacity / total_consumption
+        max_minutes_flyable: float = fuel_tank_capacity / total_consumption
 
         return Response({
             'Totale Consumption Per Minute': total_consumption,
--- airplanes/models.py
+++ airplanes/models.py
@@ -1,6 +1,6 @@
-from typing_extensions import Required
 from django.db import models
 from django.core.validators import MinValueValidator
+from rest_framework.exceptions import ValidationError
 
 
 class Airplane(models.Model):
@@ -11,3 +11,10 @@ class Airplane(models.Model):
         validators=[MinValueValidator(0.0)], default=0.8)
     passenger_fuel_increase = models.FloatField(
         validators=[MinValueValidator(0.0)], default=0.002)
+
+    def save(self, *args, **kwargs):
+        if (Airplane.objects.count() >= 10):
+            raise ValidationError(
+                'You can not add another airplane instance, limit reached.')
+        else:
+            super(Airplane, self).save(*args, **kwargs)
