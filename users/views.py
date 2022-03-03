ee45b7e78e7e1ebfe96515fb88342f11c4d0f684
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2022-03-03 14:57:16 +0100

    test fetch

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 
 class ViewA(generics.GenericAPIView):
@@ -7,3 +8,9 @@ class ViewA(generics.GenericAPIView):
 
 class ViewC(generics.GenericAPIView):
     pass
+
+
+class ViewAPI(generics.GenericAPIView):
+
+    def get(self, request):
+        return Response({"test": "test"})
